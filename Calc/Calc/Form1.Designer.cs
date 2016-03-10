8385ad33d70f116d6cc3a119776d1be80c8ff420
Nikita2903 <nikita13.10@mail.ru>
2016-03-10 12:58:01 +0600

    REr

--- Calc/Calc/Form1.Designer.cs
+++ Calc/Calc/Form1.Designer.cs
@@ -103,7 +103,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 231);
+            this.ClientSize = new System.Drawing.Size(568, 231);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
