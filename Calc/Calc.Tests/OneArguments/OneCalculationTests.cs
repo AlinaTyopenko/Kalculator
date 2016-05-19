ceb1a534f70c917d2e485f73d7fbdc31d1051c90
Nikita2903 <nikita13.10@mail.ru>
2016-05-19 13:12:08 +0600

    NewTest

---
+++ Calc/Calc.Tests/OneArguments/OneCalculationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Calc.OneArguments;
+using NUnit.Framework;
+
+namespace Calc.Tests.OneArguments
+{
+    public class OneCalculationTests
+    {
+        [TestCase("button5", typeof(Sin))]
+        [TestCase("button9", typeof(Tan))]
+        [TestCase("button10", typeof(Binary))]
+        [TestCase("button11", typeof(Cube))]
+        [TestCase("button6", typeof(Cos))]
+        [TestCase("button7", typeof(Sqrt))]
+        [TestCase("button8", typeof(Exp))]
+        [TestCase("button12", typeof(Log))]
+        [TestCase("button14", typeof(Ugoldannogocos))]
+        public void FactoryTest(string name, Type type)
+        {
+            IOneCalculation calc = OneCalculation.CreateCalculator(name);
+            Assert.IsInstanceOf(type,calc);
+        }
+    }
+}
---
+++ Calc/Calc.Tests/TwoArguments/TwoArgumentFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Calc.OneArguments;
+using Calc.TwoArguments;
+using NUnit.Framework;
+
+namespace Calc.Tests.TwoArguments
+{
+    public class TwoArgumentFactoryTests
+    {
+        [TestCase("button5", typeof(Sin))]
+        [TestCase("button9", typeof(Tan))]
+        [TestCase("button10", typeof(Binary))]
+        [TestCase("button11", typeof(Cube))]
+        [TestCase("button6", typeof(Cos))]
+        [TestCase("button7", typeof(Sqrt))]
+        [TestCase("button8", typeof(Exp))]
+        [TestCase("button12", typeof(Log))]
+        [TestCase("button14", typeof(Ugoldannogocos))]
+        public void FactoryTest(string name, Type type)
+        {
+            ITwoArgumentsCalculator calc = TwoArguments.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calc);
+        }
+    }
+}
